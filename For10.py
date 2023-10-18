1b5d721a902adffba36d023280048983a66d4f6f
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-17 23:11:08 -0400

    Return the l.append(list1[i].capitalize())

--- For10.py
+++ For10.py
@@ -6,4 +6,10 @@ def main(list1):
     Returns:
         list: return  answer
     """
-    return
+    i=0
+    l=[]
+    while i<len(list1):
+        l.append(list1[i].capitalize())
+        i+=1
+    return l
+print(main(['rustam', 'diyor', 'alisher', 'bektosh']))
